02a8b4470e0153df0a4294c1d9dd0fe259d33a22
ninazmul <nazmulsaw@gmail.com>
2024-01-12 11:03:29 +0600

    second task done

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { FaPaste } from "react-icons/fa6";
 import { AiFillLike } from "react-icons/ai";
 import { AiOutlineComment } from "react-icons/ai";
@@ -6,6 +7,22 @@ import { MdOutlineDateRange } from "react-icons/md";
 
 const Cart = ({ item }) => {
   const { clientName, desc } = item;
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    setUploadedFiles(files);
+  };
+
   return (
     <div className="card w-72 glass m-1 shadow-xl">
       <div className="card-body">
@@ -26,16 +43,49 @@ const Cart = ({ item }) => {
           <button>
             <AiOutlineComment />
           </button>
-          <button>
-            <GrAttachment />
-          </button>
+
+          <div>
+            <input
+              type="file"
+              onChange={handleFileChange}
+              multiple
+              className="hidden"
+              id="fileInput"
+            />
+            <label htmlFor="fileInput">
+              <GrAttachment />
+            </label>
+          </div>
           <button>
             <MdOutlineDateRange />
           </button>
         </div>
+
+        {isModalOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <h2>Attachment Modal</h2>
+              <button onClick={closeModal}>Close Modal</button>
+            </div>
+          </div>
+        )}
+        {uploadedFiles.length > 0 && (
+          <div>
+            <h3>Uploaded Files:</h3>
+            <ul>
+              {Array.from(uploadedFiles).map((file, index) => (
+                <li key={index}>{`${file.name} (${file.type})`}</li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default Cart;
+
+<div className="card-actions flex justify-around items-center">
+  
+</div>;
